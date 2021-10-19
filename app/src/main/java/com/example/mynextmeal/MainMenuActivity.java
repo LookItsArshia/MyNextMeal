95304377b3622922243859dd42dbbba9878e32db
AndyfMaxwell <andyfmaxwell@gmail.com>
2021-10-18 20:23:48 -0400

    added activity files

---
+++ app/src/main/java/com/example/mynextmeal/MainMenuActivity.java
@@ -0,0 +1,24 @@
+package com.example.mynextmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void goToScan(View view) {
+
+    }
+
+    public void goToMyMenu(View view) {
+
+    }
+
+
+}
